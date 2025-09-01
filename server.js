8195c270e8c8b85b772791c674df2d2f059561ef
Anderson Guimarães <user.email>
2025-09-01 09:07:08 -0300

    Remoção de comentários no server.js

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
 
 
 const express = require('express');
-// Importa os arquivos de rotas que criamos
+// Importa os arquivos das rotas
 const alunoRotas = require('./rotas/alunoRotas');
 const professorRotas = require('./rotas/professorRotas');
 
 const app = express();
-const port = 3001; // Mantendo sua porta original
+const port = 3001; 
 
-// Middleware para que o Express entenda dados de formulário (x-www-form-urlencoded)
+//
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware para que o Express entenda JSON. É uma boa prática ter ambos.
+
 app.use(express.json());
 
-// Diz ao aplicativo para usar o arquivo de rotas de alunos
-// para qualquer endereço que comece com /alunos
+
 app.use('/alunos', alunoRotas);
 
-// Diz ao aplicativo para usar o arquivo de rotas de professores
-// para qualquer endereço que comece com /professores
+
 app.use('/professores', professorRotas);
 
-// Inicia o servidor e o faz "escutar" por requisições na porta definida
+// Inicia o servidor 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
